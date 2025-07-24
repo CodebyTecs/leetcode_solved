ea491e690fd3e1a48e9a52343be938d37caad248
Tecs <va.pogoreloff@gmail.com>
2025-07-24 10:14:02 +0700

    Time: 4 ms (48.92%) | Memory: 6.3 MB (35.70%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.go
@@ -0,0 +1,18 @@
+func isPalindrome(x int) bool {
+    if x < 0 {
+        return false
+    }
+
+    s := strconv.Itoa(x)
+
+    left, right := 0, len(s) - 1
+    for left < right {
+        if s[left] != s[right] {
+            return false
+        }
+        left++
+        right--
+    }
+
+    return true
+}
