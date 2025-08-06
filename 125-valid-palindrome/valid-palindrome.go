38e6e889b91f978f1bbc171681040a43274f9bc5
Tecs <va.pogoreloff@gmail.com>
2025-08-06 12:38:19 +0700

    Time: 0 ms (100.00%) | Memory: 6.4 MB (16.46%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.go
@@ -0,0 +1,22 @@
+import (
+    "unicode"
+)
+
+func isPalindrome(s string) bool {
+    var filtered []rune
+    for _, ch := range s {
+        if unicode.IsLetter(ch) || unicode.IsDigit(ch) {
+            filtered = append(filtered, unicode.ToLower(ch))
+        }
+    }
+
+    n := len(filtered)
+    for i := 0; i < n/2; i++ {
+        if filtered[i] != filtered[n-1-i] {
+            return false
+        }
+    }
+
+    return true
+
+}
