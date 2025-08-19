8bd5e3a73faf27ec6f259b81cc5cc66732c2d8b3
Tecs <va.pogoreloff@gmail.com>
2025-08-19 13:58:44 +0700

    Time: 0 ms (100.00%) | Memory: 3.9 MB (100.00%) - LeetSync

---
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.go
@@ -0,0 +1,67 @@
+type MyQueue struct {
+    inStack []int
+    outStack []int
+}
+
+
+func Constructor() MyQueue {
+    return MyQueue {
+        inStack: []int{},
+        outStack: []int{},
+    }
+}
+
+
+func (this *MyQueue) Push(x int)  {
+    this.inStack = append(this.inStack, x)
+}
+
+
+func (this *MyQueue) Pop() int {
+    if len(this.outStack) == 0 {
+        for len(this.inStack) > 0 {
+            x := this.inStack[len(this.inStack) - 1]
+            this.inStack = this.inStack[:len(this.inStack) - 1]
+            this.outStack = append(this.outStack, x)
+        }
+    }
+
+    num := this.outStack[len(this.outStack) - 1]
+    this.outStack = this.outStack[:len(this.outStack) - 1]
+
+    return num
+}
+
+
+func (this *MyQueue) Peek() int {
+    if len(this.outStack) == 0 {
+        for len(this.inStack) > 0 {
+            x := this.inStack[len(this.inStack) - 1]
+            this.inStack = this.inStack[:len(this.inStack) - 1]
+            this.outStack = append(this.outStack, x)
+        }
+    }
+
+    num := this.outStack[len(this.outStack) - 1]
+
+    return num
+}
+
+
+func (this *MyQueue) Empty() bool {
+    if len(this.inStack) == 0 && len(this.outStack) == 0 {
+        return true
+    }
+
+    return false
+}
+
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Push(x);
+ * param_2 := obj.Pop();
+ * param_3 := obj.Peek();
+ * param_4 := obj.Empty();
+ */
