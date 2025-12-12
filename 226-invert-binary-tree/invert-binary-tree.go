de11c344578484c5aacb1c2433275c5531b63629
Tecs <va.pogoreloff@gmail.com>
2025-12-12 10:31:17 +0700

    Time: 0 ms (100.00%) | Memory: 4.1 MB (56.34%) - LeetSync

---
+++ 226-invert-binary-tree/invert-binary-tree.go
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func invertTree(root *TreeNode) *TreeNode {
+    if root == nil {
+        return nil
+    }
+
+    right := invertTree(root.Right)
+    left := invertTree(root.Left)
+    root.Right = left
+    root.Left = right
+
+    return root
+}
