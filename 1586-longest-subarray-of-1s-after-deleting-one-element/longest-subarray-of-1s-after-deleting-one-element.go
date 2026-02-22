40a893bf4ec5646b5de714b12452920679654545
Tecs <va.pogoreloff@gmail.com>
2026-02-22 11:45:27 +0700

    Time: 0 ms (100.00%) | Memory: 9.8 MB (33.08%) - LeetSync

---
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.go
@@ -0,0 +1,24 @@
+func longestSubarray(nums []int) int {
+    left := 0
+    zeroCount := 0
+    maxLen := 0
+
+    for right := 0; right < len(nums); right++ {
+        if nums[right] == 0 {
+            zeroCount++
+        }
+
+        for zeroCount > 1 {
+            if nums[left] == 0 {
+                zeroCount--
+            }
+            left++
+        }
+
+        if right-left > maxLen {
+            maxLen = right - left
+        }
+    }
+
+    return maxLen
+}
