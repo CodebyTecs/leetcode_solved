615131dbc6ccf8b2b1e8d1b69a7af39bc8faffb0
Tecs <va.pogoreloff@gmail.com>
2026-02-26 19:59:02 +0700

    Time: 3 ms (40.21%) | Memory: 6.1 MB (18.00%) - LeetSync

--- 1-two-sum/two-sum.go
+++ 1-two-sum/two-sum.go
@@ -1,13 +1,18 @@
 func twoSum(nums []int, target int) []int {
-    res := []int{}
-    for i := 0; i < len(nums); i++ {
-        for j := i + 1; j < len(nums); j++{
-            if nums[i] + nums[j] == target {
-                res = append(res, i, j)
-                return res
+    hash := make(map[int]int, len(nums))
+    for i, v := range nums {
+        hash[v] = i
+    }
+    
+    for i, n := range nums {
+        j := target - n
+        if i != hash[j] {
+            _, ok := hash[j]
+            if ok {
+                return []int{i, hash[j]}
             }
         }
     }
-
-    return res
+    
+    return nil
 }
