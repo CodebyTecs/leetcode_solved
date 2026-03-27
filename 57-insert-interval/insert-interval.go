709ed271940f691eff2697e1bcb9b2510f899935
Tecs <va.pogoreloff@gmail.com>
2026-03-27 12:25:43 +0700

    Time: 0 ms (100.00%) | Memory: 6 MB (63.03%) - LeetSync

---
+++ 57-insert-interval/insert-interval.go
@@ -0,0 +1,29 @@
+func insert(intervals [][]int, newInterval []int) [][]int {
+    res := make([][]int, 0)
+    i := 0
+    n := len(intervals)
+
+    for i < n && intervals[i][1] < newInterval[0] {
+        res = append(res, intervals[i])
+        i++
+    }
+
+    for i < n && intervals[i][0] <= newInterval[1] {
+        if intervals[i][0] < newInterval[0] {
+            newInterval[0] = intervals[i][0]
+        }
+        if intervals[i][1] > newInterval[1] {
+            newInterval[1] = intervals[i][1]
+        }
+        i++
+    }
+
+    res = append(res, newInterval)
+
+    for i < n {
+        res = append(res, intervals[i])
+        i++
+    }
+
+    return res
+}
